c8b4b3b368abd7d55c7dca86c081c8fe3961c562
xinyong <xinyong@shandagames.com>
2020-07-21 20:12:47 +0800

    开始重构

---
+++ include/container/shm_vector.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <string>
+#include <list>
+#include "shm_string.h"
+#include "shm_obj.h"
+#include "../mem_alloc/alloc.h"
+
+namespace smd {
+
+template <class T>
+class ShmList : public ShmObj {
+public:
+	ShmList(Alloc& alloc, const std::string& name = "")
+		: ShmObj(ShmObj::ObjType::OBJ_LIST)
+		, m_alloc(alloc)
+		, m_name(alloc, name) {}
+
+	//
+	// 注意，析构函数里面不能调用clear()，需要使用者主动调用来回收共享内存
+	//
+	~ShmList() {}
+
+	std::list<T>& GetList() { return m_list; }
+
+	bool empty() { return m_list.empty(); }
+	size_t size() { return m_list.size(); }
+	void clear() { m_list.clear(); }
+
+private:
+	Alloc& m_alloc;
+	ShmString m_name;
+	std::list<T> m_list;
+};
+
+} // namespace smd
--- include/mem_alloc/alloc.h
+++ include/mem_alloc/alloc.h
@@ -2,6 +2,78 @@
 #include "buddy.h"
 
 namespace smd {
+/*
+**空间配置器，以变量数目为单位分配
+*/
+template <class T>
+class allocator {
+public:
+	typedef T value_type;
+	typedef T* pointer;
+	typedef const T* const_pointer;
+	typedef T& reference;
+	typedef const T& const_reference;
+	typedef size_t size_type;
+	typedef ptrdiff_t difference_type;
+
+public:
+	T* allocate();
+	T* allocate(size_t n);
+	void deallocate(T* ptr);
+	void deallocate(T* ptr, size_t n);
+
+	void construct(T* ptr);
+	void construct(T* ptr, const T& value);
+	void destroy(T* ptr);
+	void destroy(T* first, T* last);
+
+private:
+	const void* m_basePtr;
+	const size_t m_offSet;
+	SmdBuddyAlloc::buddy* m_buddy;
+	size_t m_used = 0;
+};
+
+template <class T>
+T* allocator<T>::allocate() {
+	return static_cast<T*>(alloc::allocate(sizeof(T)));
+}
+template <class T>
+T* allocator<T>::allocate(size_t n) {
+	if (n == 0)
+		return 0;
+	return static_cast<T*>(alloc::allocate(sizeof(T) * n));
+}
+template <class T>
+void allocator<T>::deallocate(T* ptr) {
+	alloc::deallocate(static_cast<void*>(ptr), sizeof(T));
+}
+template <class T>
+void allocator<T>::deallocate(T* ptr, size_t n) {
+	if (n == 0)
+		return;
+	alloc::deallocate(static_cast<void*>(ptr), sizeof(T) * n);
+}
+
+template <class T>
+void allocator<T>::construct(T* ptr) {
+	new (ptr) T();
+}
+template <class T>
+void allocator<T>::construct(T* ptr, const T& value) {
+	new (ptr) T(value);
+}
+template <class T>
+void allocator<T>::destroy(T* ptr) {
+	ptr->~T();
+}
+template <class T>
+void allocator<T>::destroy(T* first, T* last) {
+	for (; first != last; ++first) {
+		first->~T();
+	}
+}
+
 class Alloc {
 public:
 	Alloc(void* ptr, size_t off_set, unsigned level)
@@ -24,6 +96,18 @@ public:
 	void Free(void* addr, size_t size) {
 		m_used -= size;
 		SmdBuddyAlloc::buddy_free(m_buddy, (int)((size_t)addr - m_offSet - (int64_t)m_basePtr));
+	}
+
+	template<class T>
+	T* New() {
+		auto t = (T*)Malloc(sizeof(T));
+		t->T();
+	}
+
+	template <class T>
+	void Delete(T* p) {
+		p->~T();
+		Free(p, sizeof(T));
 	}
 
 	size_t GetUsage() const {
