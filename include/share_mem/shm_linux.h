2ca21ba15ef907a2434f2c56973794d6a50d8731
xinyong <xinyong@shandagames.com>
2020-09-16 17:37:12 +0800

    更新Linux下共享内存的访问方式

--- include/share_mem/shm_linux.h
+++ include/share_mem/shm_linux.h
@@ -38,58 +38,33 @@ public:
 
 	void* acquire(const std::string& fmt_name, std::size_t size, ShareMemOpenMode mode) {
 		name_ = fmt_name;
-		fd_ = ::shm_open(
-			name_.c_str(), O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+		size_ = calc_size(size);
+		shm_id_ = shmget(fmt_name, size, 0640 | IPC_CREAT);
 
 		if (mode == kCreateAlways) {
-			if (fd_ == 0) {
-				::shm_unlink(name_.c_str());
+			if (shm_id_ == 0) {
+				if (shmctl(shm_id, IPC_RMID, nullptr) < 0) {
+					m_log.DoLog(
+						Log::LogLevel::kError, "fail shmctl IPC_RMID[%d]: %s\n", errno, name_.c_str());
+					return nullptr;
+				}
 			}
 
-			fd_ = ::shm_open(name_.c_str(), O_CREAT | O_EXCL | O_RDWR,
-					S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-
-			if (fd_ < 0) {
-				m_log.DoLog(Log::LogLevel::kError, "fail shm_open[%d]: %s\n", errno, name_.c_str());
+			shm_id_ = shmget(fmt_name, size_, 0640 | IPC_CREAT | IPC_EXCL);
+			if (shm_id_ < 0) {
+				m_log.DoLog(Log::LogLevel::kError, "fail shmget IPC_EXCL [%d]: %s\n", errno, name_.c_str());
 				return nullptr;
 			}
-
-			size_ = calc_size(size);
-			if (::ftruncate(fd_, static_cast<off_t>(size_)) != 0) {
-				m_log.DoLog(Log::LogLevel::kError, "fail ftruncate[%d]: %s, size = %zd\n", errno,
-					name_.c_str(), size_);
-				return nullptr;
-			}
-
 		} else {
-			if (fd_ < 0) {
-				m_log.DoLog(Log::LogLevel::kError, "fail shm_open[%d]: %s\n", errno, name_.c_str());
-				return nullptr;
-			}
-
-			struct stat st;
-			if (::fstat(fd_, &st) != 0) {
-				m_log.DoLog(Log::LogLevel::kError, "fail fstat[%d]: %s, size = %zd\n", errno,
-					name_.c_str(), size_);
-				return nullptr;
-			}
-			size_ = static_cast<std::size_t>(st.st_size);
-			if ((size_ <= sizeof(info_t)) || (size_ % sizeof(info_t))) {
-				m_log.DoLog(Log::LogLevel::kError, "fail to_mem: %s, invalid size = %zd\n",
-					name_.c_str(), size_);
-				return nullptr;
-			}
-
-			if (size_ < size) {
-				m_log.DoLog(Log::LogLevel::kError, "fail to_mem: %s, invalid size = %zd\n",
-					name_.c_str(), size_);
+			if (shm_id_ < 0) {
+				m_log.DoLog(Log::LogLevel::kError, "fail shmget IPC_CREAT [%d]: %s\n", errno, name_.c_str());
 				return nullptr;
 			}
 		}
 
-		mem_ = ::mmap(nullptr, size_, PROT_READ | PROT_WRITE, MAP_SHARED, fd_, 0);
-		if (mem_ == MAP_FAILED) {
-			m_log.DoLog(Log::LogLevel::kError, "fail mmap[%d]: %s, size = %zd\n", errno,
+		mem_ = shmat(shm_id_, nullptr, 0);
+		if (mem_ == reinterpret_cast<void*>(-1)) {
+			m_log.DoLog(Log::LogLevel::kError, "fail shmat[%d]: %s, size = %zd\n", errno,
 				name_.c_str(), size_);
 			return nullptr;
 		}
@@ -100,13 +75,13 @@ public:
 
 	void release() {
 		if (mem_ != nullptr && size_ > 0) {
-			::munmap(mem_, size_);
+			shmdt(mem_);
 		}
 	}
 
 private:
 	Log& m_log;
-	int fd_ = -1;
+	int shm_id_ = -1;
 	void* mem_ = nullptr;
 	std::size_t size_ = 0;
 	std::string name_;
